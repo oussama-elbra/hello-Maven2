a117a0c13b79cadc3e4d61d6b9bcd83305a0dd48
ELBARHMI <elbarhmi.oussama@gmail.com>
2022-10-10 10:37:30 +0200

    first commit

---
+++ src/main/java/hello_maven/Afficher.java
@@ -0,0 +1,16 @@
+package hello_maven;
+
+public class Afficher {
+	
+	
+	/**
+	 * 
+	 * test commit and push
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Paris");
+	}
+	
+	
+}
